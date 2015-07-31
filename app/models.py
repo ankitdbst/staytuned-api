39e087a57c3c7ef46f1d04126214cebbebb7cafe
Ankit Gupta <mailtoankitgupta@gmail.com>
2015-07-31 12:31:36 +0530

    Set up

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-
-class Channel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-
---
+++ project/models.py
@@ -0,0 +1,7 @@
+from project import db
+
+
+class Channel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
