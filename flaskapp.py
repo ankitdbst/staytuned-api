e0c49df1bc035a7e24a4f8bc2a0a2d053527a82b
Ankit Gupta <mailtoankitgupta@gmail.com>
2015-08-17 01:06:25 +0530

    Test

--- flaskapp.py
+++ flaskapp.py
@@ -5,16 +5,6 @@ from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
 
 
-@app.route('/')
-def index():
-    return render_template('templates/index.html')
-
-
-@app.route('/<path:resource>')
-def serveStaticResource(resource):
-    return send_from_directory('static/', resource)
-
-
 @app.route("/test")
 def test():
     return "<strong>It's Alive!</strong>"
