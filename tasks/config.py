671fd0a2da87d657178bb9adb80e1f849f5864e2
Ankit Gupta <mailtoankitgupta@gmail.com>
2015-10-27 16:23:56 +0530

    Rename database to "stay"

--- tasks/config.py
+++ tasks/config.py
@@ -3,7 +3,7 @@ import os
 MONGO_DB_HOST = os.environ.get('OPENSHIFT_MONGODB_DB_HOST', '127.0.0.1')
 MONGO_DB_PORT = os.environ.get('OPENSHIFT_MONGODB_DB_PORT', '27017')
 
-MONGO_DB_NAME = os.environ.get('OPENSHIFT_MONGODB_DB_NAME', 'staytuned')
+MONGO_DB_NAME = os.environ.get('OPENSHIFT_MONGODB_DB_NAME', 'stay')
 MONGO_USER_NAME = os.environ.get('OPENSHIFT_MONGODB_USER_NAME', 'admin')
 MONGO_USER_PASS = os.environ.get('OPENSHIFT_MONGODB_USER_PASS', 'admin')
 
